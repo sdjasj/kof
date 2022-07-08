28e160e90603532593f5d9f1f435b7736dd38fca
sdjasj <1594576288@qq.com>
2022-07-08 09:40:33 +0800

    5

--- static/js/kyo.js
+++ static/js/kyo.js
@@ -13,7 +13,7 @@ export class kyo extends Player {
     let offsets = [0, -22, -22, -140, 0, 0, 0];
     for (let i = 0; i < 7; i++) {
       let gif = GIF();
-      gif.load(`../img/player/kyo/${i}.gif`);
+      gif.load(`static/img/player/kyo/${i}.gif`);
       this.animations.set(i, {
         gif: gif,
         frame_cnt: 0,  // 总图片数
